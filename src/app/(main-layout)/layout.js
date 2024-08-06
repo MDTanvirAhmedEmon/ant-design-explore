e25527987885fd26375b1536635c19e7849c8273
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2024-08-07 00:35:42 +0600

    set up layout

--- src/app/(main-layout)/layout.js
+++ src/app/(main-layout)/layout.js
@@ -1,6 +1,13 @@
+import { ConfigProvider } from "antd";
 
 export default function MainLayout({ children }) {
   return (
+    <ConfigProvider
+      theme={{
+        token: {},
+      }}
+    >
       <body>{children}</body>
+    </ConfigProvider>
   );
 }
--- src/app/(main-layout)/page.js
+++ src/app/(main-layout)/page.js
@@ -1,11 +1,23 @@
+import { Flex, Layout } from 'antd';
+import { Content, Footer, Header } from 'antd/es/layout/layout';
+import Sider from 'antd/es/layout/Sider';
 import React from 'react';
 
 const Home = () => {
-    return (
-        <div>
-            <h1>This Is Home</h1>
-        </div>
-    );
+  return (
+    <Flex>
+      <Layout>
+        <Sider width="25%">
+          Sider
+        </Sider>
+        <Layout>
+          <Header>Header</Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </Layout>
+    </Flex>
+  );
 };
 
 export default Home;
